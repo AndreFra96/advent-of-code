4e490b0a1135bbbc9bdbd837f6ad8df111f3e340
ALA TECH <alatech@Mac.station>
2025-12-14 11:32:40 +0100

    2025 day 9 part 1

---
+++ 2025/day_9/part_1/script.ts
@@ -0,0 +1,20 @@
+import fs from "fs"
+
+const input = fs.readFileSync('./input.txt', 'utf-8')
+
+const coordinates = input.split("\n").map((row) => row.split(",").map(Number))
+let largest = 0;
+for (let i = 0; i < coordinates.length; i++) {
+    for (let j = i + 1; j < coordinates.length; j++) {
+        const first = coordinates[i]
+        const second = coordinates[j]
+        const sideOne = (first[0] > second[0] ? first[0] - second[0] : second[0] - first[0]) + 1
+        const sideTwo = (first[1] > second[1] ? first[1] - second[1] : second[1] - first[1]) + 1
+        const size = sideOne * sideTwo
+        if (size > largest) {
+            largest = size
+        }
+    }
+}
+
+console.log("Dimensione massima", largest)
